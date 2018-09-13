9be6ea473844ba3c43756b6559c7231a48869a34
puncherw <43150359+puncherw@users.noreply.github.com>
2018-09-13 18:59:14 +0300

    first commit  Please enter the commit message for your changes. Lines starting

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function warmup(temperature) {
-  // your implementation here
-};
+module.exports =  function warmup(temperature) {
+let  fahrenheit=temperature*1.8+32;
+
+return  fahrenheit;
+
+}
+    
+
